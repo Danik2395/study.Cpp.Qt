85310cc8dc3204a52495879408495332129b7937
Danik2395 <daniil.kalenovich@gmail.com>
2026-09-15 20:02:51 +0300

    feat(lab3): add lab3
    - Fix Prison
    - Add get_exp_cells_num() in Prison

--- labs/Lab3Widget.h
+++ labs/Lab3Widget.h
@@ -5,7 +5,10 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QString>
+#include <QValidator>
 #include <qboxlayout.h>
+#include <qlabel.h>
 #include <qnamespace.h>
 #include <qobject.h>
 #include <new>
@@ -13,6 +16,10 @@
 #include "../Interfaces.h"
 #include "../ui/ui_Lab3Widget_panel.h"
 #include "../ui/ui_Lab3Widget_show.h"
+#include "classes/Prison.h"
+
+constexpr size_t PRISON_CELLS_COUNT = 15;
+constexpr int LAB3_MAX_CRIMINALS = 10;
 
 namespace Ui
 {
@@ -27,19 +34,58 @@ class Prison_Show_Widget : public QWidget
 
     Ui::Prison_Show* prison_show_ui;
 
-    friend class Lab3Widget;
-
 public:
-    Prison_Show_Widget(QWidget* parent = nullptr) : QWidget(parent), prison_show_ui(nullptr)
+    Prison_Show_Widget(Prison* prison, QWidget* parent = nullptr) :
+        QWidget(parent),
+        prison_show_ui(nullptr),
+        prison(prison)
     {
         prison_show_ui = new Ui::Prison_Show;
         prison_show_ui->setupUi(this); // Method to build the widget tree on the parent widget
+        label_criminals_cnt = prison_show_ui->label_criminals_cnt;
+        label_crouded_cells_nums = prison_show_ui->label_crouded_cells_nums;
+        label_exp_criminals_cell_nums = prison_show_ui->label_exp_criminals_cell_nums;
+
+        label_criminals_cnt->setProperty("criminal_templ", label_criminals_cnt->text() + QString(" %1"));
+        label_crouded_cells_nums->setProperty("criminal_templ", label_crouded_cells_nums->text() + QString(" %1"));
+        label_exp_criminals_cell_nums->setProperty("criminal_templ", label_exp_criminals_cell_nums->text() + QString(" %1"));
+        update_labels();
     }
 
     ~Prison_Show_Widget() { delete prison_show_ui; }
+
+    void update_labels()
+    {
+        QString text_count_to_set;
+
+        text_count_to_set = QString::number(prison->get_criminals_count());
+        label_criminals_cnt->setText(label_criminals_cnt->property("criminal_templ").toString().arg(text_count_to_set));
+
+        auto create_num_comma_str = [&text_count_to_set](ssstl::Vector<int>&& nums){
+            text_count_to_set.clear();
+            for (int num : nums)
+            {
+                text_count_to_set += " " + QString::number(num) + ", ";
+            }
+            if (!text_count_to_set.isEmpty()) text_count_to_set.chop(2);
+        };
+
+        create_num_comma_str(prison->get_crouded_cells_num());
+        label_crouded_cells_nums->setText(label_crouded_cells_nums->property("criminal_templ").toString().arg(text_count_to_set));
+
+        create_num_comma_str(prison->get_exp_cells_num());
+        label_exp_criminals_cell_nums->setText(label_exp_criminals_cell_nums->property("criminal_templ").toString().arg(text_count_to_set));
+    }
+
+private:
+
+    Prison* prison;
+    QLabel* label_criminals_cnt;
+    QLabel* label_crouded_cells_nums;
+    QLabel* label_exp_criminals_cell_nums;
 };
 
-class Lab3Widget : public QWidget, public ILabWindow
+class Lab3Widget : public QWidget, public ILabWindow, Threaded<void>
 {
     Q_OBJECT
 
@@ -51,20 +97,55 @@ signals:
     void close_requested();
 
 public:
-    Lab3Widget(QString name = "Lab", QWidget* parent = nullptr) : QWidget(parent), prison_show_widget(nullptr)
+    Lab3Widget(QString name = "Lab", QWidget* parent = nullptr) :
+        QWidget(parent),
+        prison_show_widget(nullptr),
+        prison(PRISON_CELLS_COUNT)
     {
         prison_panel_ui = new Ui::Prison_Panel;
         prison_panel_ui->setupUi(this);
 
+        auto* employees_validator = new QIntValidator(0, LAB3_MAX_CRIMINALS, this);
+        prison_panel_ui->edit_cell_11->setValidator(employees_validator);
+        prison_panel_ui->edit_cell_12->setValidator(employees_validator);
+        prison_panel_ui->edit_cell_13->setValidator(employees_validator);
+        prison_panel_ui->edit_cell_14->setValidator(employees_validator);
+
         connect(prison_panel_ui->btn_random_gen,
                 &QPushButton::clicked,
                 this,
-                &Lab3Widget::randomgen_prison);
+                [this](){
+                prison.random_fill();
+                if (!open_prison_show_widget())
+                {
+                    prison_show_widget->update_labels();
+                }
+                });
 
         connect(prison_panel_ui->btn_show_prison,
                 &QPushButton::clicked,
                 this,
                 &Lab3Widget::open_prison_show_widget);
+
+        connect(prison_panel_ui->edit_cell_11,
+                &QLineEdit::returnPressed,
+                this,
+                [this](){ set_special_cell_cnt_panel(Prison::FRST); });
+
+        connect(prison_panel_ui->edit_cell_12,
+                &QLineEdit::returnPressed,
+                this,
+                [this](){ set_special_cell_cnt_panel(Prison::SEC); });
+
+        connect(prison_panel_ui->edit_cell_13,
+                &QLineEdit::returnPressed,
+                this,
+                [this](){ set_special_cell_cnt_panel(Prison::THRD); });
+
+        connect(prison_panel_ui->edit_cell_14,
+                &QLineEdit::returnPressed,
+                this,
+                [this](){ set_special_cell_cnt_panel(Prison::FRTH); });
     }
 
     ~Lab3Widget() { delete prison_panel_ui; }
@@ -76,25 +157,55 @@ public:
 
 private:
 
-    void open_prison_show_widget()
-    {
-        if (prison_show_widget) return;
+    Prison prison;
 
-        prison_show_widget = new Prison_Show_Widget(this);
+    // Return 1 if widget exists
+    // Return 0 if doesn't
+    int open_prison_show_widget()
+    {
+        if (prison_show_widget) return 0;
+
+        prison_show_widget = new Prison_Show_Widget(&prison, this);
 
         prison_show_widget->setAttribute(Qt::WA_DeleteOnClose);
         prison_show_widget->setParent(this, Qt::Dialog);
 
         connect(prison_show_widget, &QObject::destroyed, this, [this]() { prison_show_widget = nullptr; });
 
+        // prison_show_widget->init_prison();
+
         prison_show_widget->show();
         prison_show_widget->raise();
         prison_show_widget->setFocus();
+
+        return 1;
     }
 
-    void randomgen_prison()
+    void set_special_cell_cnt_panel(Prison::Spec_Cells_Num cell_num)
     {
+        QLineEdit* target_edit;
 
+        switch (cell_num)
+        {
+            case Prison::FRST: target_edit = prison_panel_ui->edit_cell_11;   break;
+            case Prison::SEC:  target_edit = prison_panel_ui->edit_cell_12;   break;
+            case Prison::THRD: target_edit = prison_panel_ui->edit_cell_13;   break;
+            case Prison::FRTH: target_edit = prison_panel_ui->edit_cell_14;   break;
+            default: return;
+        }
+
+        bool ok_int = false;
+        int criminals_count = target_edit->text().toInt(&ok_int);
+        if (ok_int)
+        {
+            prison.set_special_cell_cnt(cell_num, criminals_count);
+
+            if (prison_show_widget)
+            {
+                prison_show_widget->update_labels();
+            }
+        }
+
+        target_edit->clear();
     }
 };
-
--- labs/classes/Prison.h
+++ labs/classes/Prison.h
@@ -33,7 +33,9 @@ public:
 
 private:
     ssstl::Vector<Cell> prison;
+    ssstl::Vector<Cell> filtered_prison;
     ssstl::Vector<int> special_cells_crimnals_count;
+    int criminals_count;
 
     ssstl::random<int> exp_dist;
     ssstl::random<int> name_dist;
@@ -44,16 +46,23 @@ public:
 
     Prison(size_t cells_cnt) :
         prison(cells_cnt),
+        special_cells_crimnals_count(cells_cnt),
         exp_dist(0, 100),
         name_dist(0, 3),
         lvl_dist(0, 255),
         criminals_dist(0, 10)
     {}
 
+    int get_criminals_count()
+    {
+        return criminals_count;
+    }
+
     void random_fill()
     {
         std::string names[] = {"Alex", "Peter", "Andrew", "Jordan"};
 
+        criminals_count = 0;
 
         for (int n = 0; n < prison.size(); ++n)
         {
@@ -61,7 +70,7 @@ public:
 
             Cell rand_cell;
 
-            if (n >= FRST || n <= FRTH)
+            if (n >= FRST && n <= FRTH)
             {
                 rand_cell = Cell(special_cells_crimnals_count[n]);
             }
@@ -77,16 +86,17 @@ public:
                 criminal = {
                     .place_active = true,
                     .experience = exp_dist,
-                    .name = names[name_dist],
+                    .name = names[name_dist()],
                     .level_in_the_skyrim = lvl_dist,
                 };
+                ++criminals_count;
             }
         }
     }
 
     void set_special_cell_cnt(Spec_Cells_Num num, int count)
     {
-        special_cells_crimnals_count[num] = count;
+        special_cells_crimnals_count.at(num) = count;
     }
 
     void add_prisoner(int cell_num, std::string& name)
@@ -94,6 +104,7 @@ public:
         try
         {
             prison.at(cell_num).emplace_back(true, exp_dist, name, lvl_dist);
+            ++criminals_count;
         }
         catch (std::out_of_range)
         {
@@ -106,6 +117,7 @@ public:
         try
         {
             prison.at(cell_num) = {};
+            --criminals_count;
         }
         catch (std::out_of_range)
         {
@@ -115,11 +127,14 @@ public:
 
     // couded will firder out all not crouded cells,
     // exp    will firder out all non experienced criminals
-    ssstl::Vector<Cell> get_filtered_prisoners(bool crouded, bool exp)
+    const ssstl::Vector<Cell>* get_filtered_prisoners(bool crouded, bool exp)
     {
-        ssstl::Vector<Cell> quiery_pr = prison;
+        if (!crouded && !exp) return &prison;
+        // Possible to not filter if comparing via Pair but it's without move semantics
 
-        for (auto& cell : quiery_pr)
+        filtered_prison = prison;
+
+        for (auto& cell : filtered_prison)
         {
             if (crouded)
             {
@@ -144,7 +159,7 @@ public:
             }
         }
 
-        return quiery_pr; // No std::move() because of NRVO
+        return &filtered_prison;
     }
 
     ssstl::Vector<int> get_crouded_cells_num()
@@ -155,6 +170,30 @@ public:
             if (prison[n].size() <= CROUDED_SIZE) continue;
             crouded_cells_num.emplace_back(n);
         }
-        return crouded_cells_num;
+        return crouded_cells_num; // No std::move() because of NRVO
+    }
+
+    ssstl::Vector<int> get_exp_cells_num()
+    {
+        ssstl::Vector<int> exp_cells_num;
+        for (int n = 0; n < prison.size(); ++n)
+        {
+            bool cell_exp = false;
+            for (const auto& criminal : prison[n])
+            {
+                if (criminal.experience          <= EXP_EXP ||
+                    criminal.level_in_the_skyrim <= EXP_SKYRIM)
+                {
+                    continue;
+                }
+                cell_exp = true;
+                break;
+            }
+            if (cell_exp)
+            {
+                exp_cells_num.push_back(n); // The same for int with both emplace_back and push_back
+            }
+        }
+        return exp_cells_num;
     }
 };
